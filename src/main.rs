21e62d97de6e317713a96f5f01b1914c5219e882
Jeremy Soller <jeremy@system76.com>
2019-09-17 21:24:41 -0600

    init

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use hwio::{Io, Pio};
+use std::io;
+
+fn main() {
+    if unsafe { libc::iopl(3) } != 0 {
+        panic!("iopl: {}", io::Error::last_os_error());
+    }
+
+    let addr_port = || -> Pio<u8> {
+        Pio::<u8>::new(0x2E)
+    };
+
+    let data_port = || -> Pio<u8> {
+        Pio::<u8>::new(0x2F)
+    };
+
+    let superio_read = |reg: u8| -> u8 {
+        addr_port().write(reg);
+        data_port().read()
+    };
+
+    let superio_write = |reg: u8, value: u8| {
+        addr_port().write(reg);
+        data_port().write(value);
+    };
+
+    let d2_read = |reg: u8| -> u8 {
+        superio_write(0x2E, reg);
+        superio_read(0x2F)
+    };
+
+    let d2_write = |reg: u8, value: u8| {
+        superio_write(0x2E, reg);
+        superio_write(0x2F, value);
+    };
+
+    let i2ec_read = |addr: u16| -> u8 {
+        d2_write(0x11, (addr >> 8) as u8);
+        d2_write(0x10, addr as u8);
+        d2_read(0x12)
+    };
+
+    println!(
+        "id {:>02X}{:>02X} rev {}",
+        i2ec_read(0x2000),
+        i2ec_read(0x2001),
+        i2ec_read(0x2002)
+    );
+
+    for bank in b'A'..=b'J' {
+        let i = (bank - b'A') as u16;
+        let data = i2ec_read(0x1601 + i);
+        let mirror = i2ec_read(0x1661 + i);
+        let pot = i2ec_read(0x1671 + i);
+        for pin in 0..8 {
+            let ctrl = i2ec_read(0x1610 + i * 8 + pin);
+            println!(
+                "{}{}: data {} mirror {} pot {} control {:02X}",
+                bank as char,
+                pin,
+                (data >> pin) & 1,
+                (mirror >> pin) & 1,
+                (pot >> pin) & 1,
+                ctrl,
+            )
+        }
+    }
+}
