f0bf26d577ab51094d03bfb56eea402533a6975c
Jeremy Soller <jackpot51@gmail.com>
2024-03-06 12:19:36 -0700

    Get GCR register data

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use hwio::{Io, Pio};
 use std::io;
 
 fn main() {
-    assert!(unsafe { libc::iopl(3) } == 0, "iopl: {}", io::Error::last_os_error());
+    assert!(
+        unsafe { libc::iopl(3) } == 0,
+        "iopl: {}",
+        io::Error::last_os_error()
+    );
 
-    let addr_port = || -> Pio<u8> {
-        Pio::<u8>::new(0x2E)
-    };
+    let addr_port = || -> Pio<u8> { Pio::<u8>::new(0x2E) };
 
-    let data_port = || -> Pio<u8> {
-        Pio::<u8>::new(0x2F)
-    };
+    let data_port = || -> Pio<u8> { Pio::<u8>::new(0x2F) };
 
     let superio_read = |reg: u8| -> u8 {
         addr_port().write(reg);
@@ -85,6 +85,38 @@ fn main() {
                 (mirror >> pin) & 1,
                 ctrl,
             )
+        }
+    }
+
+    {
+        for (address, name) in &[
+            (0x1600, "GCR"),
+            (0x16F0, "GCR1"),
+            (0x16F1, "GCR2"),
+            (0x16F2, "GCR3"),
+            (0x16F3, "GCR4"),
+            (0x16F4, "GCR5"),
+            (0x16F5, "GCR6"),
+            (0x16F6, "GCR7"),
+            (0x16F7, "GCR8"),
+            (0x16F8, "GCR9"),
+            (0x16F9, "GCR10"),
+            (0x16FA, "GCR11"),
+            (0x16FB, "GCR12"),
+            (0x16FC, "GCR13"),
+            (0x16FD, "GCR14"),
+            (0x16FE, "GCR15"),
+            (0x16E0, "GCR16"),
+            (0x16E1, "GCR17"),
+            (0x16E2, "GCR18"),
+            //TODO: only do these on IT5570 and IT5571
+            (0x16E4, "GCR19"),
+            (0x16E5, "GCR20"),
+            (0x16E6, "GCR21"),
+            (0x16E7, "GCR22"),
+            (0x16E8, "GCR23"),
+        ] {
+            println!("{}: 0x{:02X}", name, i2ec_read(*address));
         }
     }
 
